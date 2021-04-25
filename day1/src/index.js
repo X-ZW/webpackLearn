accddf51976ae7a26f2326b2e9924b1b25f85a23
X-ZW <875523627@qq.com>
2021-04-25 22:33:32 +0800

    【es6转es5，class编译，装饰器，config3】

--- day1/src/index.js
+++ day1/src/index.js
@@ -1,5 +1,19 @@
-console.log('hello webpack')
+// console.log('hello webpack')
 
-console.log(__dirname);
-require('./index.css')
-require('./index.less')
+// console.log(__dirname);
+// require('./index.css')
+// require('./index.less')
+
+
+// config3 es6 => es5
+const fun = () => 2
+fun()
+
+@log
+class A {
+    a = 2
+}
+
+function log(target) {
+    console.log(target);
+}
